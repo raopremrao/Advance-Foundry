a221c202c7f7327e8410e995880de2eb44db77dc
raopremrao <raom61337@gmail.com>
2025-12-30 13:01:32 +0530

    Foundry Setup

--- Foundry-NFTs/src/BasicNFT.sol
+++ Foundry-NFTs/src/BasicNFT.sol
@@ -1,4 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract BasicNFT {}
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNFT is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNFT() public {}
+
+    function tokenURI(uint256 tokenId) public view returns (string memory) {
+        return "";
+    }
+}
